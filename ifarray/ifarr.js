b1b0eef3386dce5e9e5be593a2781178919ecc02
Nizakat Ali <122658148+Nizakat78@users.noreply.github.com>
2024-03-15 20:29:14 +0500

    Add files via upload

---
+++ ifarray/ifarr.js
@@ -0,0 +1,33 @@
+/* let b: number = 10 , c = 50
+
+if(!(b > c)){
+    console.log(`${b} is greater then ${c}`) // this is executed  because the condition in if statement becomes false
+    console.log(`${c} is greate then ${b}`)
+}
+else if(b == c){
+    console.log(`${c}  is equal is  ${b}`)
+}
+else{
+    console.log(`${b} is less then ${c}`)
+} */
+/*
+let nam1 : string = "4577", nam2  : string = "6790"
+
+if(nam1>nam2){
+    console.log(` Hello ${nam1}`)  // nam1 is greater then name 2 this is not executed
+}
+else  if (nam1==nam2){
+    console.log(`Hello ${nam2}`) //  nam1 is equal to name2 it is not executed
+}
+ else{
+    console.log(`Both are Same`)  // this is Executed  because the first two conditions were false so it came
+}
+*/
+var arr = [3, 8, 10];
+var arr1 = [3, 5, 7];
+if (arr > arr1) {
+    console.log("First vaule of array is greater then 1st value");
+}
+else {
+    console.log("This is executed ");
+}
---
+++ ifarray/ifarr.ts
@@ -0,0 +1,35 @@
+/* let b: number = 10 , c = 50
+
+if(!(b > c)){
+    console.log(`${b} is greater then ${c}`) // this is executed  because the condition in if statement becomes false
+    console.log(`${c} is greate then ${b}`)
+}
+else if(b == c){
+    console.log(`${c}  is equal is  ${b}`)
+}
+else{
+    console.log(`${b} is less then ${c}`)
+} */
+/*
+let nam1 : string = "4577", nam2  : string = "6790"
+
+if(nam1>nam2){
+    console.log(` Hello ${nam1}`)  // nam1 is greater then name 2 this is not executed 
+}
+else  if (nam1==nam2){
+    console.log(`Hello ${nam2}`) //  nam1 is equal to name2 it is not executed 
+}
+ else{
+    console.log(`Both are Same`)  // this is Executed  because the first two conditions were false so it came
+} 
+*/
+let arr : number[] = [3,8,10] 
+let arr1: number[]= [3,5,7]
+
+if(arr>arr1){
+    console.log(`First vaule of array is greater then 1st value`) // this is exceute  beacuse all elements in both arrays are not
+}
+
+else {
+    console.log(`This is executed `)
+}
